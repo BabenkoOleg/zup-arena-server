805dcdc439cbbb7f233476357c98cdce7940a887
Kravchuk <majerkravchuk@gmail.com>
2018-10-25 19:42:56 +0300

    Add errorHandler

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
+import errorHandler from 'errorhandler';
 import app from './app';
 import logger from './util/logger';
+
+app.use(errorHandler());
 
 const server = app.listen(app.get('port'), () => {
   logger.debug(`App is running at http://localhost:${app.get('port')} in ${app.get('env')} mode`);
