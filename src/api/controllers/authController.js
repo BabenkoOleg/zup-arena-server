3411d5f79db40fe4c11abb4d502ded9b684dfb4c
Kravchuk <majerkravchuk@gmail.com>
2018-10-28 19:41:16 +0300

    Add HTTP statuses to responses

--- src/api/controllers/authController.js
+++ src/api/controllers/authController.js
@@ -3,19 +3,34 @@ const SteamService = require('../../services/Steam');
 const { User } = require('../../db/models');
 
 module.exports.create = (req, res) => {
-  SteamService.ISteamUserAuth.AuthenticateUserTicket({
-    appid: process.env.STEAM_APP_ID,
-    ticket: req.body.ticket,
-  })
-    .then((data) => {
-      User.findOrCreate({ where: { steamId: data.response.params.steamid } })
-        .spread((record) => {
-          const token = jwt.sign({
-            steamId: record.steamId,
-            uuid: record.uuid,
-          }, process.env.JWT_SECRET);
-          res.json({ jwt: token });
-        });
+  if (req.body.ticket) {
+    SteamService.ISteamUserAuth.AuthenticateUserTicket({
+      appid: process.env.STEAM_APP_ID,
+      ticket: req.body.ticket,
     })
-    .catch(error => res.json(error));
+      .then((data) => {
+        User.findOrCreate({ where: { steamId: data.response.params.steamid } })
+          .spread((record) => {
+            const token = jwt.sign({
+              steamId: record.steamId,
+              uuid: record.uuid,
+            }, process.env.JWT_SECRET);
+            res.json({ success: true, jwt: token });
+          });
+      })
+      .catch((error) => {
+        res.status(422);
+        res.json({
+          success: false,
+          error: 'Invalid token',
+          steamError: error,
+        });
+      });
+  } else {
+    res.status(422);
+    res.json({
+      success: false,
+      error: 'Not provided token parameter',
+    });
+  }
 };
