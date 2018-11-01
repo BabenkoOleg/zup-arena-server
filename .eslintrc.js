b937513626c0777dca72161b5ca1e874540d4285
Kravchuk <majerkravchuk@gmail.com>
2018-11-01 22:28:34 +0300

    Disable consistent-return rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   "extends": "airbnb-base",
   "rules": {
     "import/no-dynamic-require": 0,
-    "no-param-reassign": 0
+    "no-param-reassign": 0,
+    "consistent-return": 0
   }
 };
