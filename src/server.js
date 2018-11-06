6ff4cddd3fb55922df82fd0bd60126553c5180b0
Kravchuk <majerkravchuk@gmail.com>
2018-11-06 23:31:33 +0300

    Handle critical server errors

--- src/server.js
+++ src/server.js
@@ -24,6 +24,17 @@ const routes = require('./routes');
 
 routes.forEach(config => server.use(config.path, config.router));
 
+server.use((error, req, res, next) => {
+  res.status(500);
+  const body = {
+    status: false,
+    error: 'Something went wrong',
+  };
+  if (process.env.NODE_ENV !== 'production') body.description = error.message;
+  res.json(body);
+  next();
+});
+
 server.listen(process.env.PORT || 3000, () => {
   logger.info(`ApiServer is running at http://localhost:${server.get('port')} in ${server.get('env')} mode`);
 });
