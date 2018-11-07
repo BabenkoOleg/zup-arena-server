b7b434af2813106ea438a3bd8439e4e6e8831319
Kravchuk <majerkravchuk@gmail.com>
2018-11-07 22:24:27 +0300

    Disable class-methods-use-this lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   "rules": {
     "import/no-dynamic-require": 0,
     "no-param-reassign": 0,
-    "consistent-return": 0
+    "consistent-return": 0,
+    "class-methods-use-this": 0,
   }
 };
