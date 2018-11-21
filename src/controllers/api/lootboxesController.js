24544e8c0ad1209374fcab077aa5cf51eae23fa2
Kravchuk <majerkravchuk@gmail.com>
2018-11-21 20:01:52 +0300

    Update docs

--- src/controllers/api/lootboxesController.js
+++ src/controllers/api/lootboxesController.js
@@ -2,7 +2,7 @@ const Lootbox = require('../../models/Lootbox');
 const te = require('../../util/throwErrorWithStatus');
 
 /**
- * @api {post} /api/matches Request create Match
+ * @api {post} /api/lootboxes Request Lootboxes list
  * @apiName CreateMatch
  * @apiVersion 0.1.0
  * @apiGroup Match
@@ -14,20 +14,18 @@ const te = require('../../util/throwErrorWithStatus');
  *     "Authorization": "Bearer xxx.zzz.yyy"
  *   }
  *
- * @apiParam {Array} users List of user's steamIds or Array of lists of user's steamIds
- *
- * @apiParamExample {json} Request-Example
- *   {
- *     "users": [["12345678901234567"], ["12345678901234568"]]
- *   }
- *
  * @apiSuccess {String} id Unique match ID
  *
  * @apiSuccessExample Success-Response:
  *   HTTP/1.1 200 OK
- *     {
- *       "id": "5bde329b36dea19ab15d6ddb"
- *     }
+ *     [
+ *       {
+ *         "id": "5bf58b2661dafb4f79142672",
+ *         "steamId": "951",
+ *         "name": "COLOR",
+ *         "price": 250
+ *       },
+ *     ]
  */
 
 module.exports.index = async (request, response) => {
