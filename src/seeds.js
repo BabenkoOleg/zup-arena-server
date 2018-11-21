d617ba70bbb0760bb5707eee6e575e068fdd0878
Kravchuk <majerkravchuk@gmail.com>
2018-11-21 19:44:48 +0300

    Add seeds script

---
+++ src/seeds.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const loadVariables = require('./util/env');
+const initDb = require('./db');
+const logger = require('./util/logger');
+
+const Lootbox = require('./models/Lootbox');
+
+loadVariables();
+initDb();
+
+const lootboxes = [
+  { steamId: 951, name: 'COLOR', price: 250 },
+  { steamId: 952, name: 'FACE', price: 500 },
+  { steamId: 953, name: 'HAT', price: 750 },
+  { steamId: 954, name: 'ACCESSORY', price: 500 },
+  { steamId: 955, name: 'EMOTION', price: 250 },
+  { steamId: 960, name: 'RARE', price: 5000 },
+];
+
+lootboxes.forEach(async (lootbox) => {
+  const query = { name: lootbox.name };
+  const update = { price: lootbox.price, steamId: lootbox.steamId };
+  const options = { upsert: true, new: true, setDefaultsOnInsert: true };
+
+  try {
+    await Lootbox.findOneAndUpdate(query, update, options);
+  } catch (error) {
+    logger.error(error.message);
+  }
+});
