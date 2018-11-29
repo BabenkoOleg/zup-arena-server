eb86b6e28c6650b40ed3b77b028bf7f82cbcf89a
Kravchuk <majerkravchuk@gmail.com>
2018-11-29 20:17:35 +0300

    Add Admin model

---
+++ src/models/Admin.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const { Schema } = mongoose;
+
+const schema = new Schema({
+  login: {
+    type: String,
+    required: true,
+    index: true,
+    unique: true,
+  },
+  password: String,
+  encryptedPassword: String,
+});
+
+schema.pre('save', async function () {
+  if (this.password) {
+    this.encryptedPassword = await bcrypt.hash(this.password, 10);
+    this.password = null;
+  }
+});
+
+const Admin = mongoose.model('Admin', schema);
+
+module.exports = Admin;
